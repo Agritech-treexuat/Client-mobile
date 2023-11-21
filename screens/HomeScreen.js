f8ec7c7ead3e339277e44c708054e159872eba5a
Huy0110 <duchuy01102001@gmail.com>
2023-11-21 19:17:11 +0700

    Add example navigation screens

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from "@react-navigation/native";
 
@@ -12,6 +12,11 @@ const HomeScreen = () => {
   return (
     <View>
       <Text className="text-red-500 my-10">HomeScreen</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("ItemScreen")}
+      >
+          <Text className="text-black text-[36px] font-semibold">Go</Text>
+      </TouchableOpacity>
     </View>
   )
 }
