752196a06c640f49c4229630caa7ba5cb0395271
Huy0110 <duchuy01102001@gmail.com>
2023-11-22 15:38:21 +0700

    Add react-native-paper

--- App.js
+++ App.js
@@ -2,17 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { BottomNavigation, PaperProvider } from 'react-native-paper';
 import HomeScreen from './screens/HomeScreen'
 import ItemScreen from './screens/ItemScreen'
+import BottomMenuNavigation from './components/BottomMenuNavigation';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="ItemScreen" component={ItemScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Page" component={BottomMenuNavigation} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="ItemScreen" component={ItemScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
